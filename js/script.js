ed50b8ee62f95dda12d6898ee6194ae2b8556c3a
Kubi <jakuub33@wp.pl>
2021-03-26 12:07:14 +0100

    Section Matches

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 // DOMContentLoaded - załadowanie elementów na stronie
 document.addEventListener('DOMContentLoaded', function () {
     const nav = document.querySelector('.navbar');
+    const navLinkItems = document.querySelectorAll('.nav-link');
+    const navList = document.querySelector('.navbar-collapse');
 
     function addShadowScroll() {
         if (window.scrollY >= 300) {
@@ -17,7 +19,10 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
-    window.addEventListener('scroll', addShadowScroll); //Za każdym razem, gdy użytkownik scrolluje:
+    //Dla każdego elementu (nav-link) nasłuchuje kliknięcia, jeśli wykryję to usuwa .show (chowa menu hamburgerowe)
+    navLinkItems.forEach(item => item.addEventListener('click', () => navList.classList.remove('show')));
+
+    window.addEventListener('scroll', addShadowScroll); //Za każdym razem, gdy użytkownik scrolluje
 
     nav.addEventListener('click', addShadowBars); //Przełącznik dodawania cienia po wciśnięciu menu hamburgerowego
 })
